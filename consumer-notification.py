f228de6241b677ee4449a3dea8502472f714ac37
lookbooking <112570823+lookbooking@users.noreply.github.com>
2020-07-27 07:54:53 +0000

    delete old version

--- consumer-notification.py
+++ consumer-notification.py
@@ -12,3 +12,5 @@ if __name__ == '__main__':
     consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
                              bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
     for msg in consumer:
+        record = json.loads(msg.value)
+        calories = int(record['calories'])
