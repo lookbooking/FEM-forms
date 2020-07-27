e3f05fb99d7d361c60de530aa804f15f127a7efc
lookbooking <112570823+lookbooking@users.noreply.github.com>
2020-07-27 06:50:13 +0000

    Bump Version.

--- consumer-notification.py
+++ consumer-notification.py
@@ -6,3 +6,9 @@ from kafka import KafkaConsumer
 
 if __name__ == '__main__':
     parsed_topic_name = 'parsed_recipes'
+    # Notify if a recipe has more than 200 calories
+    calories_threshold = 200
+
+    consumer = KafkaConsumer(parsed_topic_name, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for msg in consumer:
