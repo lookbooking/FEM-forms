51eff0c81771bfe1c3e88fd1b0aece1d338c0aa5
lookbooking <112570823+lookbooking@users.noreply.github.com>
2020-12-11 21:47:25 +0000

    fixed cache

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -8,4 +8,5 @@ from kafka import KafkaProducer
 def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
-        value_bytes = bytes(value, encoding='utf-8'
+        value_bytes = bytes(value, encoding='utf-8')
+ 
