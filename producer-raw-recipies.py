2e6ab615e01a8dbd228238ed6dbb06593be8e66a
lookbooking <112570823+lookbooking@users.noreply.github.com>
2020-12-11 15:03:21 +0000

    Updated: strings.xml

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -8,4 +8,4 @@ from kafka import KafkaProducer
 def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
-        value_bytes = bytes(value, encoding
+        value_bytes = bytes(value, encoding='ut
