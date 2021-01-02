62d9a0bcee444e6c1e2da9ae42707d3454613af3
lookbooking <112570823+lookbooking@users.noreply.github.com>
2021-01-02 21:49:16 +0000

    DM9000B: driver initialization upgrade

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -9,4 +9,4 @@ def publish_message(producer_instance, topic_name, key, value):
     try:
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
-        producer_instance.send(topic_name, key=
+        producer_instance.send(topic_name, key=key_by
