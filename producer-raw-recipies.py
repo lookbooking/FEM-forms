e43914a0bc4faa49d9b723e585aa8b2abd8c26a3
lookbooking <112570823+lookbooking@users.noreply.github.com>
2021-01-08 14:05:25 +0000

    Adding screenshots

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -10,4 +10,4 @@ def publish_message(producer_instance, topic_name, key, value):
         key_bytes = bytes(key, encoding='utf-8')
         value_bytes = bytes(value, encoding='utf-8')
         producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
-        producer_i
+        producer_instan
