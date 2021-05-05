6720ba34804d1675c32a41db40cd3987bb7b92ff
lookbooking <112570823+lookbooking@users.noreply.github.com>
2021-05-05 07:24:46 +0000

    Reorganized folders

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -20,4 +20,4 @@ def publish_message(producer_instance, topic_name, key, value):
 def connect_kafka_producer():
     _producer = None
     try:
-        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], ap
