536918327070ba57281ddf33b8750670d41e4c60
lookbooking <112570823+lookbooking@users.noreply.github.com>
2021-11-16 06:48:36 +0000

    Add some more logging

--- producer-raw-recipies.py
+++ producer-raw-recipies.py
@@ -44,4 +44,4 @@ def fetch_raw(recipe_url):
 
 def get_recipes():
     recipies = []
-    salad_url = 'https://www.allrecipes.com/recipes/96/salad
+    salad_url = 'https://www.allrecipes.com/recipes/96/salad/'
