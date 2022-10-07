90a14b9c5cb08ea5b53271d3c0acb720383a3e1c
lookbooking <112570823+lookbooking@users.noreply.github.com>
2022-10-07 18:09:33 +0000

    Inicio

--- producer_consumer_parse_recipes.py
+++ producer_consumer_parse_recipes.py
@@ -8,3 +8,7 @@ from kafka import KafkaConsumer, KafkaProducer
 
 def publish_message(producer_instance, topic_name, key, value):
     try:
+        key_bytes = bytes(key, encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
+        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.flush()
