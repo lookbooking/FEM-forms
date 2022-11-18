eb99bc9e550e2ffc986c08b6a3eb90c4f774253e
lookbooking <112570823+lookbooking@users.noreply.github.com>
2022-11-18 20:18:59 +0000

    Version upped

--- producer_consumer_parse_recipes.py
+++ producer_consumer_parse_recipes.py
@@ -86,3 +86,8 @@ if __name__ == '__main__':
     parsed_topic_name = 'parsed_recipes'
 
     consumer = KafkaConsumer(topic_name, auto_offset_reset='earliest',
+                             bootstrap_servers=['localhost:9092'], api_version=(0, 10), consumer_timeout_ms=1000)
+    for msg in consumer:
+        html = msg.value
+        result = parse(html)
+        parsed_records.append(result)
